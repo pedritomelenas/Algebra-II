b9e55117c4f9690e47b778a40d9f603fa98c3fba
jlbueso <jlbueso@ugr.es>
2016-02-04 16:53:08 +0100

    Update Group.py

--- Grupos/absalg-new/Group.py
+++ Grupos/absalg-new/Group.py
@@ -440,14 +440,16 @@ class Group:
         if len(elems) == 0:
             raise ValueError("elems must have at least one element")
 
-        oldG = elems
+        oldG = elems|Set([self(self.e.elem)])
         while True:
-            newG = oldG | Set(a * b for a in oldG for b in oldG)
+            newG = oldG | Set([a * b for a in elems for b in oldG])
             if oldG == newG: break
             else: oldG = newG
         oldG = Set(g.elem for g in oldG)
-        return Group(oldG, self.bin_op.new_domains(oldG.cartesian(oldG), oldG, check_well_defined=False),parent=self.parent,check_ass=False,check_inv=False, identity=self.e.elem)
-    def is_cyclic(self):
+        return Group(oldG, self.bin_op.new_domains(oldG.cartesian(oldG), oldG, check_well_defined=False),  
+                     parent=self.parent,check_ass=False,check_inv=False, identity=self.e.elem)     
+        
+        def is_cyclic(self):
         """Checks if self is a cyclic Group"""
         return any(g.order() == len(self) for g in self)
 
@@ -687,6 +689,69 @@ class Group:
         terminator = ds[len(ds) - 1] 
         return terminator.is_trivial() 
    
+    def ElementaryDivisors(n):
+        def partition(n):
+            a = [0 for i in range(n + 1)]
+            k = 1
+            y = n - 1
+            while k != 0:
+                x = a[k - 1] + 1
+                k -= 1
+                while 2 * x <= y:
+                    a[k] = x
+                    y -= x
+                    k += 1
+                l = k + 1
+                while x <= y:
+                    a[k] = x
+                    a[l] = y
+                    yield a[:k + 2]
+                    x += 1
+                    y -= 1
+                a[k] = x + y
+                y = x + y - 1
+                yield a[:k + 1]
+        factored=factorint(n)
+        factored2={}
+        for i in factored.keys():
+            factored2[i]=list(partition(factored[i]))
+        E=[[[(lambda x: k**x)(i) for i in factored2[k][j]] for j in range(len(factored2[k]))] for k in factored2]
+        Econ=[[]]
+        for i in range(len(E)):
+            Econ=[a+[b] for a in Econ for b in E[i]]
+        return Econ
+   
+    def AbelianGroups(n,option="ElementaryDivisors"):
+        if option=="ElementaryDivisors":
+            E=ElementaryDivisors(n)
+        elif option=="InvariantFactors":
+            E=InvariantFactors(n)
+        else:
+            raise ValueError("The option is ElementaryDivisors (defect) or InvariantFactors")
+        Res=[[CyclicGroup(m) for m in flatten(h)] for h in E]
+        l=Res[0]
+        def car(l):
+            G=l[0]
+            if len(l)==1:
+                return G
+            return G.cartesian(car(l[1:]))
+        total=[car(l) for l in Res]
+        return total
+    
+   def InvariantFactors(n):
+        E=ElementaryDivisors(n)
+        H=[]
+        for B in E:
+            L=[]
+            A=deepcopy(B)
+            while len(A)!=0:
+                a=reduce(operator.mul,[h.pop() for h in A])
+                L.append(a)  
+                while [] in A:
+                    A.remove([])
+            H.append(L) 
+        return H
+    
 class GroupHomomorphism(Function): #we should add here check_well_defined, and check_group_axioms as options
     """
     The definition of a Group Homomorphism
