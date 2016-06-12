3a28c8b4e7ebdefbe04718fa289567fcf90c5bd8
jlbueso <jlbueso@ugr.es>
2016-06-12 08:48:03 +0200

    Add files via upload

--- Grupos/absalg-new/Set.py
+++ Grupos/absalg-new/Set.py
@@ -1,7 +1,7 @@
 """
 Implementation of Set
 """
-
+import itertools
 class Set(frozenset):
     """
     Definition of a Set
@@ -10,12 +10,20 @@ class Set(frozenset):
     1) it makes Set immutable
     2) it allows Set to contains Sets
     """
+    def __repr__(self):
+        return str(set(self))
+
+    def __str__(self):
+        return str(set(self))
+
     #def __mul__(self, other):
     def cartesian(self, other):
         """Cartesian product"""
         if not isinstance(other, Set):
             raise TypeError("One of the objects is not a set")
-        return Set((x, y) for x in self for y in other)
+        return Set(itertools.product(self,other, repeat=1))
+        #return Set((a,b) for a in self for b in other)
+
 
     def pick(self):
         """Return an arbitrary element. (The finite Axiom of Choice is true!)"""
